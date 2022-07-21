b0e5373dde53904cb20449cd7863fd81fcf85ecd
miraspectum <miraspectum@pm.me>
2022-07-22 00:01:21 +0600

    Auto translated 20000-29999 unique phrases

--- scripts/translate-unique.mjs
+++ scripts/translate-unique.mjs
@@ -75,7 +75,7 @@ async function main() {
             throw 'Unique list can`t be empty!';
         }
     
-        for (let i = 10000; i < 20000; i++) {
+        for (let i = 20000; i < 30000; i++) {
             try {
                 await translateUniqueData(uniqueList[i]);
                 console.log(`Progress: ${i}/${uniqueList.length}`);
